445a6d332a9b05ecd362d1cbbd97b20c598763b7
Franck Nijimbere <nijimbere.franck@gmail.com>
2017-07-16 04:38:50 -0700

    making this world a better place

--- app/routes/api.js
+++ app/routes/api.js
@@ -7,7 +7,7 @@ function matching_event(myevents, eventList) {
 	var matches = [];
 	for (var j = 0; j < myevents.length; j++){
 		for (var i = 0; i < eventList.length; i++){
-			if (eventList[i].active){
+			if (true){
 				console.log(eventList[i]);
 				return eventList[i];
 			}
