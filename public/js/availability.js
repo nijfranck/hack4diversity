1f6448368153a48185bbad289f7502cacca8eb68
Peter Clarke <pclarke@Peters-MacBook-Pro.local>
2017-07-15 22:28:03 -0700

    Added a basic front-end for availability page and fixed js issues w/ calendars

--- public/js/availability.js
+++ public/js/availability.js
@@ -1,8 +1,35 @@
+availabilityArray = [];
+idCounter = 0;
 
-
-$('#rangestart').calendar({
-  endCalendar: $('#rangeend')
+$('#rangeStart').calendar({
+  endCalendar: $('#rangeEnd')
 });
-$('#rangeend').calendar({
-  startCalendar: $('#rangestart')
+$('#rangeEnd').calendar({
+  startCalendar: $('#rangeStart')
+});
+
+// Clears the input fields so that the user can submit more information
+function clearInputs() {
+  $("#inputStart").val('');
+  $("#inputEnd").val('');
+}
+
+// Fades in a little message saying that adding the times was successful
+function fadeMessage() {
+  $("div.message")
+    .addClass("messageFade")
+    .text("Successfully added availability!")
+    .fadeIn(1000)
+    .fadeOut(1200);
+}
+
+$("#addThisAvailability").click(function() {
+  var dateInfo = {
+    start: $("#inputStart").val(),
+    end: $("#inputEnd").val(),
+  };
+  clearInputs();
+  fadeMessage();
+  availabilityArray.push(dateInfo);
+  // Pull information from the start and end input fields
 });
